070e919df9babf5565bcf51d968a9c5187b7b1f8
bfmvenom45 <bfmvenom45@gmail.com>
2025-09-11 18:04:17 +0300

    fix: use next/navigation useRouter in upload page

--- app/(root)/upload/page.tsx
+++ app/(root)/upload/page.tsx
@@ -3,22 +3,50 @@
 import FileInput from "@/components/FileInput"
 import FormField from "@/components/FormField"
 import { MAX_THUMBNAIL_SIZE, MAX_VIDEO_SIZE } from "@/constants";
+import { getThumbnailUploadUrl, getVideoUploadUrl, saveVideoDetails } from "@/lib/actions/video";
 import { useFileInput } from "@/lib/hooks/useFileInput";
-import { ChangeEvent, FormEvent, useState } from "react"
+import { useRouter } from "next/navigation";
 
+import { ChangeEvent, FormEvent, useEffect, useState } from "react"
 
+const uploadFileToBunny = (fiel: File, uploadUrl: string, accessKey: string) : Promise<void> => {
+    return fetch(uploadUrl, {
+        method: 'PUT',
+        headers: {
+            AccessKey: accessKey,
+            'Content-Type': fiel.type,
+        },
+        body: fiel,
+    }).then((res) => {
+        if (!res.ok) {
+            throw new Error('Failed to upload file to Bunny CDN');
+        }
+    }); 
+    
+}
 const Page = () => {
+
+    const router = useRouter();
     const [isSubmitting, setIsSubmitting] = useState(false);
+    const [videoDuration, setVideoDuration] = useState(0);
+
+    
+
     const [formData, setFormData] = useState({
         title: '',
         description: '',
         visibility: 'public',
     });
     const [error, setError] = useState("");
+    
 
     const video = useFileInput(MAX_VIDEO_SIZE); // 500 MB
     const thumbnail = useFileInput(MAX_THUMBNAIL_SIZE); // 5 MB
-
+    useEffect(() => {
+            if (video.duration !== null || 0) {
+                setVideoDuration(video.duration);
+            }
+        }, [video.duration]);
     const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
         const { name, value } = e.target;
 
@@ -41,6 +69,36 @@ const Page = () => {
                 return;
             }
             // Add your upload logic here
+            const {
+                videoId, 
+                uploadUrl: videoUploadUrl,
+                accessKey: videoAccessKey
+            } = await getVideoUploadUrl();
+            if(!videoUploadUrl || !videoAccessKey) throw new Error('Failed to get video upload URL');
+            // 1. Upload video to Bunny CDN
+            await uploadFileToBunny(video.file, videoUploadUrl, videoAccessKey);
+
+            // 2. Upload thumbnail to Bunny CDN
+             const {
+
+                uploadUrl: thumbnailUploadUrl,
+                accessKey: thumbnailAccessKey,
+                cdnUrl: thumbnailCdnUrl
+            } = await getThumbnailUploadUrl(videoId);
+
+            if(!thumbnailUploadUrl || !thumbnailAccessKey || !thumbnailCdnUrl) throw new Error('Failed to get thumbnail upload URL');
+            await uploadFileToBunny(thumbnail.file, thumbnailUploadUrl, thumbnailAccessKey);
+
+            // 3. Save video details to your database
+            await saveVideoDetails({
+                videoId,
+                thumbnailUrl: thumbnailCdnUrl,
+                ...formData,
+                visibility: formData.visibility as Visibility,
+                tags: "", // or provide an array or value as needed
+                duration: videoDuration,
+            });
+            router.push(`/video/${videoId}`); 
 
         } catch (error) {
             console.log("Error uploading form ", error);
